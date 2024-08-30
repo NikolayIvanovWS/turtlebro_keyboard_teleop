b15b89dbb093cfb26d023035e0879eadab7d9c10
Nikolay Ivanov <57194638+NikolayIvanovWS@users.noreply.github.com>
2024-08-30 13:54:45 +0300

    Update teleop_twist_keyboard.py

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 from geometry_msgs.msg import Twist
 import sys
